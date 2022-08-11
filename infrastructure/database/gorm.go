4000a97ebe0eb6712c039aadb0b832f3bb96bc7a
RYOTA-F <ry.fj0304@gmail.com>
2022-08-11 19:41:32 +0900

    [add] Infrastructure

---
+++ infrastructure/database/gorm.go
@@ -0,0 +1,37 @@
+package datadase
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	gorm "github.com/jinzhu/gorm"
+	godotnev "github.com/joho/godotenv"
+)
+
+// NewDB DBと接続する
+func NewDB() *gorm.DB {
+	// ENV読み取り
+	err := godotnev.Load(".env")
+	if err != nil {
+		fmt.Printf("読み込みに失敗しました: %v", err)
+	}
+
+	// 接続情報を設定
+	DBMS := "mysql"
+	USER := os.Getenv("DB_USER")
+	PASS := os.Getenv("DB_PASS")
+	// (localhost:3306ではなく) (コンテナ名:3306)
+	HOST := "tcp(db:3306)"
+	DBNAME := os.Getenv("DB_NAME")
+
+	CONNECT := USER + ":" + PASS + "@" + HOST + "/" + DBNAME
+
+	// DBに接続
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	gorm "tutorial-go-ddd/infrastructure/database"
+)
+
+type Test struct {
+	Id   int    `gorm:"column:id"`
+	Name string `gorm:"column:name"`
+}
 
 func main() {
-    fmt.Printf("Hello, world!\n")
+    db := gorm.NewDB()
+		fmt.Println("DB: 接続", db)
+
+		var tests []Test
+		db.Find(&tests)
+		defer db.Close()
+
+		fmt.Println("tests: ", tests)
 }
